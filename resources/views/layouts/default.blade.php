0759fdb81706315dfe0a1351f46f0d746345ee40
yamen <yamen.saa199@gmail.com>
2020-05-16 14:36:40 +0400

    Add flash file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,11 +6,16 @@
     <title>{{ config('app.name','posts2') }}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 @include('elements.navbar')
+@include('elements.flash') 
+
+
 </head>
 <body>
 
 <div class="container">
 @yield('content')
+
+
 
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
