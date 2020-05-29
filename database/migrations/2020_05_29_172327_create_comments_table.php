453b1d1aa97a4883dbb245fe6bdd8946fbb6703b
yamen <yamen.saa199@gmail.com>
2020-05-29 21:35:01 +0400

    create comments

---
+++ database/migrations/2020_05_29_172327_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increaments('id');
+            $tabel->integer('user_id')->unsigned();
+            $tabel->integer('post_id');
+            $tabel->text('body');
+            $table->timestamps();
+            $tabel->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
